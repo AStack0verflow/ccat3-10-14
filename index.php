9825f5a313ebc46f7ca41386128be2d045497e71
435demo.txt <ew52@humboldt.edu>
2014-03-10 14:31:48 -0700

    CCAT Kiosk Front page 3-10-14
    This  is the first prototype of the Front page to be view and critiqued
    by L. Graffman. 3-10-14

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <title></title>
+    <meta charset="UTF-8" />
+    <link rel='stylesheet'  href="style/style.css" type="text/css" media="all" />
+</head>
+<body>
+    <div id="panel_left">
+        <span><h1>A</h1></span>
+        <div class="project_item"><img src="http://placekitten.com/60/60"><h2>Rocket Toilet</h2></div>
+        <div class="project_item"><img src="http://placekitten.com/60/60"><h2>Recycled Bark Shampoo</h2></div>
+        <div class="project_item"><img src="http://placekitten.com/60/60"><h2>Hemp Machine</h2></div>
+        <div class="project_item"><img src="http://placekitten.com/60/60"><h2>Frog Juicer</h2></div>
+        <div class="project_item"><img src="http://placekitten.com/60/60"><h2>Solar Powered Tree</h2></div>
+        <div class="project_item"><img src="http://placekitten.com/60/60"><h2>Something With Mason Jars</h2></div>
+        <span><h1>B</h1></span>
+        <div class="project_item"><img src="http://placekitten.com/60/60"><h2>Another Hemp Project</h2></div>
+        <div class="project_item"><img src="http://placekitten.com/60/60"><h2>Portable Hemp Stove</h2></div>
+        <div class="project_item"><img src="http://placekitten.com/60/60"><h2>Automatic Solar Kiln</h2></div>
+        <div class="project_item"><img src="http://placekitten.com/60/60"><h2>Project Title Here</h2></div>
+        <div class="project_item"><img src="http://placekitten.com/60/60"><h2>Project Title Here</h2></div>
+        <div class="project_item"><img src="http://placekitten.com/60/60"><h2>Project Title Here</h2></div>
+    </div>
+    <div id="panel_right">
+        <span><h1>Maintenance Schedule</h1></span>
+    
+        <ul>
+            <li>Daily
+                <ul>
+                    <li><span>(Rocket Toilet)</span> Scrub the Rocket Toilet</li>
+                    <li><span>(Rocket Toilet)</span> Scrub the Rocket Toilet Again</li>
+                    <li><span>(Solar Powered Tree)</span> Water it</li>
+                    <li><span>(Something With Mason Jars)</span> Refill green sludge</li>
+                </ul>
+            </li>
+            <li>Weekly
+                <ul>
+                    <li><span>(Portable Hemp Stove)</span> Blaze It</li>
+                    <li><span>(Frog Juicer)</span> Debone grinder</li>
+                    <li><span>(Rocket Toiler)</span> Refill rocket fuel</li>
+                    <li><span>(Something With Mason Jars)</span> Refill Green Sludge</li>
+                </ul>
+            </li>
+            <li>Monthly</li>
+            <li>Yearly</li>
+        </ul>
+        
+    </div>
+    
+	<script src="style/javascript.js"></script>   
+</body>
+</html>
